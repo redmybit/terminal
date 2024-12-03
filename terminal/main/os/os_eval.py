9efd8e4042f0895c535cb693b830cc40ce4c32c9
redmybit <matthew.kosyanchuk@gmail.com>
2024-12-03 16:57:38 -0600

    Update os_eval.py

--- terminal/main/os/os_eval.py
+++ terminal/main/os/os_eval.py
@@ -1,1 +1,38 @@
-
+def execute(tokens, token, stack):
+    error = None
+    
+    operation = tokens[token[0] + 1]
+    inputs = []
+    
+    index = 2
+    input_token = None
+    while input_token != ";":
+        input_token = tokens[token[0] + index]
+        inputs.append(input_token)
+        index += 1
+    
+    new_token = None
+    
+    class Operation:
+        def __init__(self, name : str, line : str) -> None:
+            self.name = name
+            self.line = line
+        
+        def out(self, inputs):
+            return eval(self.line)
+        
+    ADD = Operation("add", "sum(inputs)")
+    
+    operations = [ADD]
+    
+    for checking in operations:
+        if operation == checking.name:
+            new_token = checking.out(inputs)   
+            break
+        
+        if new_token is not None:
+            break
+    
+    skip = len(inputs) + 1 # eval has a skip of inputs (+1 to account for the semi-colon)
+    
+    return new_token, skip, error
